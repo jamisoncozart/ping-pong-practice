5f6d8acb3ff0a920f5b985a860280594424997a6
jamisoncozart <jamisoncozart@gmail.com>
2020-02-02 18:18:50 -0800

    Wipe dist directory on build, add uglify-webpack-plugin dependency to condense bundle.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +10,8 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new UglifyJsPlugin(),
+    new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
       title: 'Ping Pong',
       template: './src/index.html',
